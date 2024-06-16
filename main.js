6cd7de6cc076ce2e7b94c16c119610630f1360e0
Gabriella Costa <gabx2205@gmail.com>
2024-06-16 15:24:34 -0300

    feat winner and looser

--- main.js
+++ main.js
@@ -20,9 +20,10 @@ const userOption = document.getElementById('user--option');
 const computerOption = document.getElementById('computer--option');
 const options = [scissor, paper, rock];
 const cpOption = options[Math.floor(Math.random() * options.length)];
+const result = document.getElementById('result');
 
 scissor.addEventListener('click', () => {
-    triangle.classList.add('invisible');
+    triangle.style.backgroundImage = 'none';
     paper.classList.add('invisible');
     paper.classList.remove('option--border');
     rock.classList.add('invisible');
@@ -34,10 +35,17 @@ scissor.addEventListener('click', () => {
     }
     cpOption.classList.remove('invisible');
     cpOption.classList.add('option--border');
+    if(cpOption == rock) {
+        result.textContent = 'YOU LOSE';
+    } else if (cpOption == scissor) {
+        result.textContent = 'DRAW';
+    } else {
+        result.textContent = 'YOU WIN';
+    }
 });
 
 paper.addEventListener('click', () => {
-    triangle.classList.add('invisible');
+    triangle.style.backgroundImage = 'none';
     scissor.classList.add('invisible');
     scissor.classList.remove('option--border');
     rock.classList.add('invisible');
@@ -49,9 +57,17 @@ paper.addEventListener('click', () => {
     }
     cpOption.classList.remove('invisible');
     cpOption.classList.add('option--border');
+    if(cpOption == scissor) {
+        result.textContent = 'YOU LOSE';
+    } else if (cpOption == paper) {
+        result.textContent = 'DRAW';
+    } else {
+        result.textContent = 'YOU WIN';
+    }
 });
 
 rock.addEventListener('click', () => {
+    triangle.style.backgroundImage = 'none';
     scissor.classList.add('invisible');
     scissor.classList.remove('option--border');
     paper.classList.add('invisible');
@@ -63,4 +79,11 @@ rock.addEventListener('click', () => {
     }
     cpOption.classList.remove('invisible');
     cpOption.classList.add('option--border');
+    if(cpOption == paper) {
+        result.textContent = 'YOU LOSE';
+    } else if (cpOption == rock) {
+        result.textContent = 'DRAW';
+    } else {
+        result.textContent = 'YOU WIN';
+    }
 });
